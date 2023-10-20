57eb7a34182032309e6a2d47e1c8b64301000fd1
Martin Palkov <martin.palkov@gmail.com>
2023-10-20 17:10:06 +0200

    add ball game code

---
+++ ball-game.js
@@ -0,0 +1,8 @@
+if (event.key === "keyW")
+    directionYball = "up";
+else if (event.key === "keyS");
+    directionXball === "down";
+else if (event.key === "keyA")
+    directionYball = "left";
+else if (event.key === "keyD");
+    directionXball === "right";
